fa608206b1d75b8b222c3775dcdb43bf86967e93
Connor Leech <connorleech@gmail.com>
2017-10-12 17:49:59 -0700

    images and parallax scroll to home screen

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,27 @@
 <template>
-<div>
-<h1>Home</h1>
+<div style='width:100%;height:100vh;'>
+	<v-parallax src="./src/assets/play-violin-1.jpeg" height='500'>
+		<v-layout column align-center justify-center>
+			<h1 class="grey--text text--darken-3">Hi my name is X</h1>
+			<h5>Jessica is a violinist and composer based out of Mill Valley, CA currently applying to colleges</h5>
+			<v-btn color="secondary">About</v-btn>
+		</v-layout>
+	</v-parallax>
+	<v-parallax src="./src/assets/violin-2.jpeg" height='500'>
+		<v-layout column align-center justify-center>
+			<h1 class="grey--text text--darken-3">View compositions</h1>
+			<h4 class="white--text">Jessica has composed dozens of pieces for X, Y and Z</h4>
+			<v-btn color="secondary">Compositions</v-btn>
+		</v-layout>
+	</v-parallax>
+	<v-parallax src="./src/assets/play-violin-2.jpeg" height='500' :jumbotron='true'>
+		<v-layout column align-center justify-center>
+			<h1 class="grey--text text--darken-3">Welcome to the website</h1>
+			<h4 class="white--text">Reach out to get in touch</h4>
+			<v-btn color="secondary">Contact</v-btn>
+
+		</v-layout>
+	</v-parallax>
 </div>
 </template>
 
